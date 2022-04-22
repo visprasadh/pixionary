620714fd70f075f0965ac9ab4b1d78507a1b907d
vis <visprasadhkumaravelu@gmail.com>
2022-04-22 17:41:53 +0530

    Rainbow

--- lib/colorButton.dart
+++ lib/colorButton.dart
@@ -3,16 +3,21 @@ import 'package:flutter/material.dart';
 class ColorButton extends StatelessWidget {
   final VoidCallback onPress;
   final Color color;
-  const ColorButton({required this.onPress, required this.color, Key? key}) : super(key: key);
+  const ColorButton({required this.onPress, required this.color, Key? key})
+      : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return GestureDetector(
       onTap: onPress,
       child: Container(
+        margin: const EdgeInsets.symmetric(horizontal: 10),
+        decoration: BoxDecoration(
+          shape: BoxShape.circle,
+          color: color,
+        ),
         height: 30,
         width: 30,
-        color: color,
       ),
     );
   }
--- lib/homePage.dart
+++ lib/homePage.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:pixionary/artboard.dart';
+import 'package:pixionary/colorButton.dart';
 import 'package:pixionary/tile.dart';
 
 class HomePage extends StatefulWidget {
@@ -11,66 +12,99 @@ class HomePage extends StatefulWidget {
 
 class _HomePageState extends State<HomePage> {
   bool enabled = false;
-  Color color = Colors.yellow;
+  Color color = Colors.purple;
+
+  void colorChange(Color c) {
+    setState(() {
+      color = c;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: Padding(
-      padding: const EdgeInsets.all(10.0),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: [
-          const Text(
-            'PIXIONARY',
-            style: TextStyle(
-              fontSize: 36,
-              fontFamily: 'Pixeled',
-            ),
-          ),
-          ArtBoard(
+        body: Column(
+      crossAxisAlignment: CrossAxisAlignment.center,
+      children: [
+        Text(
+          'PIXIONARY',
+          style: TextStyle(
+            fontSize: 36,
+            fontFamily: 'Pixeled',
             color: color,
-            size: 5,
           ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Container(
-                margin: const EdgeInsets.all(10),
-                padding: const EdgeInsets.all(10),
-                decoration: BoxDecoration(
-                  color: Colors.grey.withOpacity(0.2),
-                  borderRadius: BorderRadius.circular(10),
-                ),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    GestureDetector(
-                      onTap: () => setState(() {
-                        color = Colors.orange;
-                      }),
-                      child: Container(
-                        height: 50,
-                        width: 50,
-                        color: Colors.orange,
-                      ),
-                    ),
-                    GestureDetector(
-                      onTap: () => setState(() {
-                        color = Colors.blue;
-                      }),
-                      child: Container(
-                        height: 30,
-                        width: 30,
-                        color: Colors.blue,
-                      ),
-                    ),
-                  ],
-                ),
+        ),
+        ArtBoard(
+          color: color,
+          size: 5,
+        ),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Container(
+              margin: const EdgeInsets.all(10),
+              padding: const EdgeInsets.all(10),
+              decoration: BoxDecoration(
+                color: Colors.grey.withOpacity(0.2),
+                borderRadius: BorderRadius.circular(10),
               ),
-            ],
-          ),
-        ],
-      ),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  ColorButton(
+                    onPress: () => colorChange(Colors.purple),
+                    color: Colors.purple,
+                  ),
+                  ColorButton(
+                    onPress: () => colorChange(Colors.indigo),
+                    color: Colors.indigo,
+                  ),
+                  ColorButton(
+                    onPress: () => colorChange(Colors.blue),
+                    color: Colors.blue,
+                  ),
+                  ColorButton(
+                    onPress: () => colorChange(Colors.green),
+                    color: Colors.green,
+                  ),
+                  ColorButton(
+                    onPress: () => colorChange(Colors.yellow),
+                    color: Colors.yellow,
+                  ),
+                  ColorButton(
+                    onPress: () => colorChange(Colors.orange),
+                    color: Colors.orange,
+                  ),
+                  ColorButton(
+                    onPress: () => colorChange(Colors.red),
+                    color: Colors.red,
+                  ),
+                  // GestureDetector(
+                  //   onTap: () => setState(() {
+                  //     color = Colors.orange;
+                  //   }),
+                  //   child: Container(
+                  //     height: 50,
+                  //     width: 50,
+                  //     color: Colors.orange,
+                  //   ),
+                  // ),
+                  // GestureDetector(
+                  //   onTap: () => setState(() {
+                  //     color = Colors.blue;
+                  //   }),
+                  //   child: Container(
+                  //     height: 30,
+                  //     width: 30,
+                  //     color: Colors.blue,
+                  //   ),
+                  // ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ],
     ));
   }
 }
